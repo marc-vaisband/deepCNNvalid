c84f1325a205f74df5db07389c6956fca9a46942
Big Computer <marc.vaisband@gmail.com>
2022-01-27 10:28:43 +0100

    merged processAnnovarFile into generatePLoci, added comments

--- data_generation/generatePLoci.py
+++ data_generation/generatePLoci.py
@@ -1,50 +1,104 @@
-import numpy as np
-from processAnnovarFile import processAnnovar
-from validatedSNPlist import get_manually_called_ploci
-import random
 import pandas as pd
 import warnings
 from pairLocus import pairLocus
-
-random.seed(42)
-np.random.seed(42)  # This is how reproducibility works, right? :/
+from validatedSNPlist import build_validatedSNPlist
 
 """
-Strategy: First build alignedBAMs for every single BAM in the phonebook; then have a 
-separate code block to mix&match them to create input tensors
+This file contains utilities for parsing ANNOVAR output files (which are comma-separated) into lists of pairLocus.
+We utilise the pandas CSV engine for convenience (to minimise dependencies, this could of course be replaced).
 """
 
-
-def get_ploci_from_annovarlist(annovarlist_path, **kwargs):
-
+def pairLocus_from_call(GL_ID: str, CL_ID: str, call: pd.Series):
     """
-    We import all the pairLoci which interest us by crawling through annovar files.
+    Constructs a pairLocus object from a single line of a pd.DataFrame representing a candidate variant.
+
+    :param GL_ID: ID of germline sample
+    :param CL_ID: ID of tumour sample
+    :param call: Candidate variant represented by ANNOVAR line in a pandas Series object.
+    :return: Instance of pairLocus
+    """
+    return pairLocus(GL_ID=GL_ID, CL_ID=CL_ID, ref=call["Ref"], alt=call["Alt"],
+                     chromosome=call["Chr"], start=call["Start"], stop=call["End"] + 1,
+                     funcrefgene=call["Func.RefGene"])
+
+def processAnnovar(GL_ID: str, CL_ID: str, annovar_path, snplist_path, cats_used: list, **kwargs):
     """
 
+    :param GL_ID: ID of germline sample
+    :param CL_ID: ID of tumour sample
+    :param annovar_path: Path to ANNOVAR output file of annotated candidate variants
+    :param snplist_path: Path to .xlsx file of manually confirmed variants, formatted as in the documentation
+                         of validatedSNPlist.build_validatedSNPlist
+    :param cats_used: List of admissible entries for the 'Func.RefGene' column of ANNOVAR output.
+    :param kwargs:
+    :return: filt_ploci, a list of pairLocus objects representing candidate variants;
+             and labels, a list of bools where 1 is a genuine mutation, and 0 is a sequencing artefact
+    """
 
-    used_cats = ["exonic", "ncRNA_exonic", "splicing", "UTR3", "UTR5",
-                 "UTR5;UTR3", "downstream", "upstream", "upstream;downstream"]
+    # First, get all calls from the annovar file
+    dtype_dict = {"Chr": str, "Start": int, "End": int}
+    all_calls = pd.read_csv(annovar_path, sep=",", dtype=dtype_dict)
 
-    all_annovars_df = pd.read_csv(annovarlist_path, sep = ";")
+    # Construct pairLocus objects from each call, then filter for categories used
+    ploci = [pairLocus_from_call(GL_ID=GL_ID, CL_ID=CL_ID, call=row) for k, row in all_calls.iterrows()]
+    filt_ploci = list(filter(lambda pl: pl.funcrefgene in cats_used, ploci))
 
+    if kwargs.get("kick_indels", False):
+        filt_ploci = list(filter(lambda pl: "-" not in [pl.ref, pl.alt], filt_ploci))  # Kick indels
+
+    # Construct list of validated mutations, get labels by associated is_present method
+    vsnpl = build_validatedSNPlist(GL_ID=GL_ID, CL_ID=CL_ID, snplist_xlsx_path=snplist_path)
+    labels = list(map(vsnpl.is_present, filt_ploci))
+    labels = list(map(int, labels))
+
+    return filt_ploci, labels
+
+
+cats_used_global = ["exonic", "ncRNA_exonic", "splicing", "UTR3", "UTR5",
+                    "UTR5;UTR3", "downstream", "upstream", "upstream;downstream"]
+# This is set as a global variable because it does not change in our application, but can of course easily be made
+# into a modular argument.
+
+
+def get_ploci_from_annovarlist(annovarlist_path, pandas_csv_kwargs=None, **kwargs):
+    """
+    Top-level function which takes a csv-format list of ANNOVAR output files and their corresponding spreadsheets of
+    confirmed calls, and returns tensorization-ready data.
+
+    :param annovarlist_path: Path to csv-formatted list of ANNOVAR output files and their corresponding spreadsheets of
+    confirmed calls. Must contain the columns:
+            GL_bam_ID: ID of germline sample
+            CL_bam_ID: ID of tumour sample
+            annovar_path: path to ANNOVAR output file
+            snplist_path: path to .xlsx file, formatted as in the documentation of validatedSNPlist.build_validatedSNPlist
+    :param pandas_csv_kwargs: kwargs to be passed to pd.read_csv in parsing the annovarlist
+    :param kwargs: Keyword arguments, passed to processAnnovar, currently only kick_indels will have an impact.
+    :return: ploci, list of variant calls represented as pairLocus objects,
+             labels, list of 0-1-labels where 1 is a genuine mutation, and 0 is a sequencing artefact
+    """
+    if pandas_csv_kwargs is None:
+        pandas_csv_kwargs = {"sep": ";"}
+
+    all_annovars_df = pd.read_csv(annovarlist_path, **pandas_csv_kwargs)
+    # We utilise the pandas CSV engine for convenience
 
     ploci = []
     data_labels = []
 
+    # We simply loop over all provided ANNOVAR files, and convert each into lists of pairLocus objects and labels.
     for GL_ID, CL_ID, annovar_path, snplist_path in zip(all_annovars_df["GL_bam_ID"], all_annovars_df["CL_bam_ID"],
                                                         all_annovars_df["annovar_path"], all_annovars_df["snplist_path"]):
         try:
 
             new_ploci, new_labels = processAnnovar(
-                GL_ID = GL_ID,
-                CL_ID = CL_ID,
-                annovar_path = annovar_path,
-                snplist_path = snplist_path,
-                cats_used = ["exonic", "ncRNA_exonic", "splicing", "UTR3", "UTR5",
-                                     "UTR5;UTR3", "downstream", "upstream", "upstream;downstream"], **kwargs)
+                GL_ID=GL_ID,
+                CL_ID=CL_ID,
+                annovar_path=annovar_path,
+                snplist_path=snplist_path,
+                cats_used=cats_used_global, **kwargs)
 
-            assert isinstance(new_labels, list)  # Just in case at some point something inexplicably becomes an array
-            assert isinstance(new_ploci, list)   # and we get horrible behaviour with the +=. Yay Python!
+            assert isinstance(new_labels, list)
+            assert isinstance(new_ploci, list)
 
             data_labels += new_labels
             ploci += new_ploci
@@ -55,32 +109,3 @@ def get_ploci_from_annovarlist(annovarlist_path, **kwargs):
 
     return ploci, data_labels
 
-
-def get_manually_called_ploci_from_annovarlist(annovarlist_path, **kwargs):
-    all_annovars_df = pd.read_csv(annovarlist_path, sep=";")
-
-    ploci = []
-    data_labels = []
-
-    for GL_ID, CL_ID, annovar_path, snplist_path in zip(all_annovars_df["GL_bam_ID"], all_annovars_df["CL_bam_ID"],
-                                                        all_annovars_df["annovar_path"],
-                                                        all_annovars_df["snplist_path"]):
-        try:
-            new_manual_ploci = get_manually_called_ploci(GL_ID = GL_ID, CL_ID = CL_ID, snplist_xlsx_path=snplist_path)
-            new_labels = [1] * len(new_manual_ploci)
-
-            assert isinstance(new_labels, list)  # Just in case at some point something inexplicably becomes an array
-            assert isinstance(new_manual_ploci, list)  # and we get horrible behaviour with the +=. Yay Python!
-
-            data_labels += new_labels
-            ploci += new_manual_ploci
-        except (ValueError, FileNotFoundError) as e:
-            warnings.warn(f"Skipping the {GL_ID} vs. {CL_ID} annovar due to the following error: {e}. "
-                          f"Script continues to next annovar.")
-            pass
-
-    return ploci, data_labels
-
-def get_same_position_ploci(pl: pairLocus, pl_list: list):
-    return [pl_ for pl_ in pl_list if
-            pl.chromosome == pl_.chromosome and pl.start == pl_.start and pl.stop == pl_.stop]
