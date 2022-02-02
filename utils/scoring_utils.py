7893c229f24c466a713da4a8a1adbf264ce5de1b
Big Computer <marc.vaisband@gmail.com>
2022-02-02 12:05:05 +0100

    Docstrings

--- utils/scoring_utils.py
+++ utils/scoring_utils.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 def score_from_num(y_true_num, y_pred_num, verbose=False):
+    """
+    Utility function that takes binary (!) true and predicted labels and converts them into accuracy, precision,
+    recall and F1 score, to evaluate prediction quality.
+
+    :param y_true_num: np.array of true numerical class labels
+    :param y_pred_num: np.array of predicted numerical class labels
+    :param verbose: bool to signify whether the scores should be printed to console
+    :return: Accuracy, Precision, Recall and F1 score calculated
+    """
+
+
     assert y_true_num.shape[0] == y_pred_num.shape[0]
     tp_mask = np.logical_and(y_pred_num == 1, y_true_num == 1)
     fp_mask = np.logical_and(y_pred_num == 1, y_true_num == 0)
@@ -23,7 +34,18 @@ def score_from_num(y_true_num, y_pred_num, verbose=False):
 
     return acc, precision, recall, f1
 
+
 def score_from_cat(y_true_cat, y_pred_cat, verbose=False):
+    """
+    Utility function that takes binary (!) true and predicted labels in categorical form and converts them into
+    accuracy, precision, recall and F1 score, to evaluate prediction quality.
+
+    :param y_true_cat: np.array of true categorical class labels
+    :param y_pred_cat: np.array of predicted categorical class labels
+    :param verbose: bool to signify whether the scores should be printed to console
+    :return: Accuracy, Precision, Recall and F1 score calculated
+    """
+
     assert y_true_cat.shape[0] == y_pred_cat.shape[0]
     assert y_true_cat.shape[1] == y_pred_cat.shape[1] == 2
  
